4b3f78005ac961c6504592c4dc12a2d7b342be40
Shashwat Ganesh <shashwatganesh@protonmail.com>
2022-06-25 13:15:51 +0530

    argv_parsing: Basic argument parsing
    Only the help message is implemented, need to implement reading the
    index file and read the given note.

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+void
+show_help()
+{
+  printf("Notes Management\n");
+  printf("-h\t Show this message\n");
+  printf("list\t List stored notes\n");
+  printf("show [title]\t Show note with given title\n");
+}
+
+struct PageEntry
+{
+  time_t created_at;
+  char *title, *description;
+};
+
+void
+show_list()
+{}
+int
+main(int argc, char** argv)
+{
+  if (argc < 3) {
+    show_help();
+    return 0;
+  } else if (strcmp(argv[3], "list") == 0) {
+    show_list();
+  } else if (strcmp(argv[3], "show") == 0) {
+    show_note(argc, argv);
+  } else {
+    show_help();
+  }
+  return 0;
+}
